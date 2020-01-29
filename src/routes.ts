8094a365fc5da00c5e2dc55892478d694fe6754e
jiajunmao <aaronmao.hangzhou@gmail.com>
2020-01-28 19:00:36 -0500

    change routes

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,7 @@ router.get("/jwt", controller.general.getJwtPayload);
 // 用于避开企业微信机器人不支持CORS的问题
 router.post("/sendText", controller.general.sendText);
 
-router.post("/git", controller.gitlab.getWebhook);
+router.post("/git", controller.github.getWebhook);
 router.post("/gitlab", controller.gitlab.getWebhook);
 
 router.post("/github", controller.github.getWebhook);
